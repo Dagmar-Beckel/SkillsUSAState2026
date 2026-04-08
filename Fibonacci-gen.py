d69e2962f5449ac4ab8e524128cc54b4c9fa89e2
Dagmar-Beckel <beckeldagmar@gmail.com>
2026-04-08 09:47:06 -0500

    Added an if statement for when the user inputs the command "--last-only". I  also moved around the variables and if statements so that they would actually be in the correct order to work and not override other commands.

--- Fibonacci-gen.py
+++ Fibonacci-gen.py
@@ -46,8 +46,9 @@ def main():
     #These variables are set up so that if statements can just check if the variable is true or false. 
     #The variables check if it's in args
     count = None
-    one_line = "--one-line" in args
+    last_only = "--last-only" in args
     numbering = "--numbering" in args
+    one_line = "--one-line" in args
 
     #if user types "-c" or "--count", the next argument, which is the number, will be read and stored in the count variable.
     if "-c" in args:
@@ -63,12 +64,19 @@ def main():
     #This calls the fibonacci function with the user entered "count".
     seq = fibonacci(count)
 
+    #If "last_only" variable is "true", the last number is the list will be printed
+    #If it's in the arguments(args), "last_only" variable is "true".
+    #Seq[-1] gives the last number in the list.
+    if last_only:
+        print(seq[-1])
+        return
+
     #If "numbering" variable is "true", a list will be made with the numbers placement and it's fibonacci number.
     #If it's in the arguments(args), "numbering" variable is "true".
     #Enumerate(seq) gives (index, value) pairs.
     if numbering:
         seq = [f"{i+1}:{num}" for i, num in enumerate(seq)]
-    
+
     #If "one_line" variable is "true", all numbers will be printed on one line operated by commas.
     #If it's in the arguments(args), "one_line" variable is "true".
     #Else, each number is printed on a separate line.
@@ -78,7 +86,6 @@ def main():
         for num in seq:
             print(num)
 
-
-    #This means that the program will run only if this file is executed.
+#This means that the program will run only if this file is executed.
 if __name__ == "__main__":
     main()
