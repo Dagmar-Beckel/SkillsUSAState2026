a86dd7ab5d0c491306b2795664d8a3f15318c1a2
Dagmar-Beckel <beckeldagmar@gmail.com>
2026-04-08 09:13:00 -0500

    I added the is statement for when the user inputs "--help"

--- Fibonacci-gen.py
+++ Fibonacci-gen.py
@@ -18,18 +18,18 @@ def print_help():
     Help for Fibonacci generator:
 	   
     --help : Print this help
-	   
+	
     --count|-c : Calculate to this many places. IE: 0, 1, 1, 2, 3, 5 would be the result of -c 6
     
-	--one-line: Print all the numbers on one line, separated by commas. Without this option,
+    --one-line: Print all the numbers on one line, separated by commas. Without this option,
     each number in the sequence will be printed on a new line.
     
-	--numbering: Preface each number in the sequence with it's placement: IE for “-c 6 
+    --numbering: Preface each number in the sequence with it's placement: IE for “-c 6 
 	--numbering –-one-line” you would get this: “1:0, 2:1, 3:1, 4:2, 5:3, 6:5” where the first
     number is the count and the second is the Fibonacci sequence. Note: this argument should
     work with all other arguments.
 
-	--last-only: Only print the last number in the sequence
+    --last-only: Only print the last number in the sequence
     """)
 
 #sys.argv is a list (array-like) of arguments from the command line.
@@ -37,6 +37,8 @@ def print_help():
 #For example, "./Fibonacci-gen -c 6 -one-line" is entered and this function will make args = ['-c', '6', '-one-line']
 args = sys.argv[1:]
 
+#This function will be called when the program is run. 
+#It will check the arguments and call the correct functions based on what the user entered.
 def main():
     #This variable will hold the count of Fibonacci numbers to generate, which will be set based on user input
     #Right now the intial value is 6.
@@ -44,6 +46,11 @@ def main():
 
     #This calls the fibonacci function with the user entered "count"
     seq = fibonacci(count)
+
+    #if user types "--help", function print_help() will be called
+    if "--help" in args:
+        print_help()
+        return
 
     #This is to test that the fibonacci function is working correctly. 
     #It will print the list of Fibonacci numbers generated.
